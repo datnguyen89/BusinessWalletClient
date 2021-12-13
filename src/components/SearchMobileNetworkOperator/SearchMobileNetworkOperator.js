c506852ed39325c9ddddeb24cf4524e879a34e35
ntdat89.hn@gmail.com <Dat29489@>
2021-12-13 10:25:12 +0700

    update scroll

--- src/components/SearchMobileNetworkOperator/SearchMobileNetworkOperator.js
+++ src/components/SearchMobileNetworkOperator/SearchMobileNetworkOperator.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react'
 import {
-  ImageProviderArea, ProviderWrapper, SearchInputPhoneNumber,
+  ImageProviderArea, ImgWrapper, ProviderWrapper, SearchInputPhoneNumber,
   TagProvider,
 } from './SearchMobileNetworkOperatorStyled'
 import { inject, observer } from 'mobx-react'
+import HorizontalScroll from 'react-scroll-horizontal'
 
 const _ = require('lodash')
 
@@ -12,34 +13,35 @@ const SearchMobileNetworkOperator = props => {
 
 
   useEffect(() => {
-    mobileNetworkOperatorStore.getServicePlanMobile();
-  }, []);
+    mobileNetworkOperatorStore.getServicePlanMobile()
+  }, [])
 
   const handlerSetSelectProvider = (value) => {
-    handleSelectedProvider(value);
+    handleSelectedProvider(value)
   }
 
   const handleOnChange = (value) => {
-    setPhoneNumber(value.target.value);
+    setPhoneNumber(value.target.value)
   }
-                         
+
   return (
     <ProviderWrapper>
-      <SearchInputPhoneNumber placeholder={"Nhập số điện thoại"} onChange={(value) => handleOnChange(value)} />
-      <TagProvider>
+      <SearchInputPhoneNumber placeholder={'Nhập số điện thoại'} onChange={(value) => handleOnChange(value)} />
+      <HorizontalScroll pageLock={true} reverseScroll={true} style={{ width: '100%', height: 76 }}>
         {
           mobileNetworkOperatorStore.mobileNetworkOperators.map(item =>
-            <ImageProviderArea onClick={() => handlerSetSelectProvider(item)} key={item.id} borderColor={item.id === selectedProvider?.id ? '#0465B0' : '#E0E0E0'}>
-              <img src={item.imageUrl} alt={item.name} />
-            </ImageProviderArea>
+            <ImageProviderArea onClick={() => handlerSetSelectProvider(item)} key={item.id}>
+              <ImgWrapper borderColor={item.id === selectedProvider?.id ? '#0465B0' : '#E0E0E0'}>
+                <img src={item.imageUrl} alt={item.name} />
+              </ImgWrapper>
+            </ImageProviderArea>,
           )
         }
-      </TagProvider>
+      </HorizontalScroll>
     </ProviderWrapper>
   )
 }
 
-SearchMobileNetworkOperator.propTypes = {
-}
+SearchMobileNetworkOperator.propTypes = {}
 
 export default inject('mobileNetworkOperatorStore')(observer(SearchMobileNetworkOperator))
--- src/components/SearchMobileNetworkOperator/SearchMobileNetworkOperatorStyled.js
+++ src/components/SearchMobileNetworkOperator/SearchMobileNetworkOperatorStyled.js
@@ -14,7 +14,7 @@ export const TitlePickProviders = styled.h5`
 
 export const SearchInputPhoneNumber = styled(Input) `
   border-radius: 10px;
-  height: 40px;
+  height: 64px;
   line-height: 40px;
 `
 
@@ -23,25 +23,35 @@ export const TagProvider = styled.div `
   overflow: hidden;
   overflow-x: auto;
 `
-
-export const ImageProviderArea = styled.div`
+export const ImgWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 60px;
+  border-radius: 10px;
+  padding: 8px 16px;
+  margin: 16px 16px 0 0;
+  width: 100%;
+  border: ${props => `1px solid ${props.borderColor}`};
+  img {
+    width: 200px;
+    object-fit: contain;
+  }
+  
+`
+export const ImageProviderArea = styled.div`  
   display: flex;
   justify-content: center;
   flex-grow: 0;     /* do not grow   - initial value: 0 */
   flex-shrink: 0;   /* do not shrink - initial value: 1 */
-  flex-basis: 150px; /* width/height  - initial value: auto */
-  border: ${props => `1px solid ${props.borderColor}`};
-  padding: 10px 20px;
-  margin: 20px 15px 10px 0;
-  color: white;
-  border-radius: 10px;
+  flex-basis: 150px; /* width/height  - initial value: auto */    
+  color: white; 
   cursor: pointer;
-  img {
-    max-width: 200px;
-    object-fit: contain;
+  &:last-child {
+    ${ImgWrapper} {
+      margin-right: 1px;
+    }
   }
 `
-
 export const ImgIconProvider = styled.div `
   margin-right: 15px;
   border: 1px solid #ccc;
